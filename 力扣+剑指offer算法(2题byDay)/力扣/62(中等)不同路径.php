a1c5a942f7c10aa5b69614b32493dd84e00728a1
wuye251 <754093136@qq.com>
2020-01-06 14:45:07 +0800

    不同路径剔除重复路径

---
+++ 力扣+剑指offer算法(2题byDay)/力扣/62(中等)不同路径.php
@@ -0,0 +1,49 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $m
+     * @param Integer $n
+     * @return Integer
+     */
+
+
+    //简单版本
+    function uniquePaths($m, $n) {
+      
+        if ($m <= 0 || $n <= 0) return 0;
+
+        if ($m == 1 && $n == 1) return 1;
+        if ($m == 2 && $n == 2) return 2;
+
+        $count  = 0;
+        $count += $this->uniquePaths($m - 1,$n);
+        $count += $this->uniquePaths($m, $n - 1);
+        
+        return $count;
+    }
+
+
+    //将已经计算过的路径条数存储  减少重复查找次数
+    public $arr;
+    function uniquePaths($m, $n) {
+      
+        if ($m <= 0 || $n <= 0) return 0;
+
+        if ($m == 1 && $n == 1) return 1;
+        if ($m == 2 && $n == 2) return 2;
+
+        if (isset($this->arr[$m][$n])) return $this->arr[$m][$n];
+
+        $this->arr[$m][$n-1] = $this->uniquePaths($m, $n-1);
+        $this->arr[$m-1][$n] = $this->uniquePaths($m-1, $n);
+        $this->arr[$m][$n] = $this->arr[$m][$n-1] + $this->arr[$m-1][$n];
+
+        return $this->arr[$m][$n];
+
+    }
+}
+
+$test = new Solution();
+print($test->uniquePaths(23,12));
