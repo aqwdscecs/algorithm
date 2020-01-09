10088079843d3947dcf99e2494f262ed5d613231
wuye251 <754093136@qq.com>
2020-01-09 16:04:46 +0800

    力扣打卡

---
+++ 力扣+剑指offer算法(2题byDay)/力扣/6.Z字形打印.php
@@ -0,0 +1,38 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @param Integer $numRows
+     * @return String
+     */
+    function convert($s, $numRows) {
+		//行数小于2时不能Z字打印
+		//字符长度小于行数时不能打印
+		//行数为1直接返回
+    	$len = strlen($s);
+		if ($len <= 1 || $len < $numRows || $numRows == 1) return $s;
+
+		$curRows = 0;
+		$direDown = 0; // 方向
+		$rowCh = [];
+
+		$chIndex = 0;
+		//放置每行的字符
+		while ($chIndex < $len) {
+
+			$rowCh[$curRows] .= $s[$chIndex];
+			//只有在第一行或者最后一行时curRow方向会改变
+			if ($curRows == 0 || $curRows == $numRows-1) $direDown = !$direDown;
+			$curRows += $direDown ? 1 : -1;
+		}
+
+		//将每行字符合并
+		$retRowStr = '';
+		for ($row=0; $row < $numRows; $row++){
+			$retRowStr .= $$rowCh[$row];
+		}
+		return $retRowStr;
+    }
+}
