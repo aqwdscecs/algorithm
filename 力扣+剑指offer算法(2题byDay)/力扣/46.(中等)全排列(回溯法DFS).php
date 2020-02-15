fe5b82c097554fa52ab9d70e632a446f25c586d0
wuye251 <754093136@qq.com>
2020-02-15 14:29:34 +0800

    回溯法 DFS入门学习 3h

---
+++ 力扣+剑指offer算法(2题byDay)/力扣/46.(中等)全排列(回溯法DFS).php
@@ -0,0 +1,40 @@
+<?php
+
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[][]
+     */
+    function permute($nums) {
+        $count = count($nums);
+        if ($count <= 1) return array($nums);
+
+        $retArr = [];
+        $this->backtrack($count, $nums, $retArr, 0);
+        return $retArr;
+    }
+
+    function backtrack($count, $nums, &$getArr, $first) {
+        if ($first == $count) {
+            $getArr[] = $nums;
+            return $getArr;
+        }
+
+        for ($i=$first; $i < $count; $i++) {
+            echo "i-->$i";echo "<br/>";
+            $this->swap($nums[$i], $nums[$first]);
+            $this->backtrack($count, $nums, $getArr, $first+1);
+            $this->swap($nums[$i], $nums[$first]);
+        }
+
+    }
+
+    function swap(&$value1, &$value2) {
+        $temp = $value1;
+        $value1 = $value2;
+        $value2 = $temp;
+    }
+    
+}
