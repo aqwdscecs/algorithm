c2ffd6f3023e28f96ae4245fec36f18d8e04f0af
wuye251 <754093136@qq.com>
2020-02-24 12:39:51 +0800

    usrot函数实现

---
+++ 力扣+剑指offer算法(2题byDay)/剑指offer/(中等)把数组排成最小的数.php
@@ -0,0 +1,16 @@
+<?php
+
+function PrintMinNumber($numbers)
+{
+    // write code here
+    usort($numbers, function($a,$b){
+       if("$a$b" > "$b$a") return 1;
+        return -1;
+    });
+    return implode("",$numbers);
+}
+
+//知识点 
+// usort的使用
+// return 1 表示需要交换
+// return -1 表示不需要变换
--- 力扣+剑指offer算法(2题byDay)/剑指offer/(中等)整数中1出现的次数（从1到n整数中1出现的次数).php
+++ 力扣+剑指offer算法(2题byDay)/剑指offer/(中等)整数中1出现的次数（从1到n整数中1出现的次数).php
@@ -22,3 +22,4 @@ function getNumberOfOne($number)
     }
     return $numCount;
 }
+
