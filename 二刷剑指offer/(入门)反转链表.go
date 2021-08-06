a72d36f1a1d7423682b6eb17a93062f5ed8d5719
wuye251 <754093136@qq.com>
2021-08-06 11:23:54 +0800

    反转链表

---
+++ 二刷剑指offer/(入门)反转链表.go
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reverseList(head *ListNode) *ListNode {
+    
+    var preHead *ListNode = nil
+    curHead := head
+    for curHead != nil {
+        nextHead := curHead.Next
+
+        curHead.Next = preHead
+        preHead = curHead
+        curHead = nextHead
+    }
+
+
+    return preHead
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    
+    if head == nil || head.Next == nil {
+        return head
+    }
+
+    newHead := reverseList(head.Next)
+
+    head.Next.Next = head
+    head.Next = nil
+    return newHead
+}
